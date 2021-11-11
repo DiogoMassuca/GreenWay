f67f6af74bc331a43892033107580b0f13b799c4
Salvador Marchante <salvasmaster@gmail.com>
2021-11-11 08:14:11 +0000

    fix

--- src/main/java/com/example/p1/models/Rate.java
+++ src/main/java/com/example/p1/models/Rate.java
@@ -2,11 +2,16 @@ package com.example.p1.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="rates")
 public class Rate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="rate_rating")private double rating;
     private User user;
     private Restaurante restaurante;
