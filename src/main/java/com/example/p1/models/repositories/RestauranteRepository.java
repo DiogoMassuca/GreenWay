be696f761e2c7efedfddfa044d47735476de90fb
Salvador Marchante <salvasmaster@gmail.com>
2021-11-18 14:25:43 +0000

    cmt

--- src/main/java/com/example/p1/models/repositories/RestauranteRepository.java
+++ src/main/java/com/example/p1/models/repositories/RestauranteRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RestauranteRepository extends CrudRepository<Restaurante, Integer>{
-        String resQuery1 = "SELECT res_name" +
-                            "FROM restaurantes, ementas, pratos, alimentos, pratoalimento" +
+        String resQuery1 = "SELECT res_name " +
+                            "FROM restaurantes, ementas, pratos, alimentos, pratoalimento " +
                             "WHERE eme_res_id = res_id AND eme_pra_id = pra_id AND pa_pra_id = pra_id AND pa_ali_id = ali_id AND ali_id <> 1";
 
                             @Query(value=resQuery1, nativeQuery=true)
