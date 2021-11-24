6efbd45f122fb3c36efdbf5534d74f7226e1a467
Salvador Marchante <salvasmaster@gmail.com>
2021-11-24 15:48:05 +0000

    c

--- src/main/java/com/example/p1/models/repositories/UserRepository.java
+++ src/main/java/com/example/p1/models/repositories/UserRepository.java
@@ -12,7 +12,7 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 
     @Modifying @Transactional
     @Query(value="insert into users (user_name, user_email, user_password, user_bdate) "+
-        "values(:#{#users.name}, :#{#users.email}, :#{users.password}, :#{users.bdate}   )", nativeQuery=true)
+        "values(:#{#users.user_name}, :#{#users.email}, :#{users.password}, :#{users.bdate}   )", nativeQuery=true)
     Integer createUser(@Param("users") User user);
  
 }
