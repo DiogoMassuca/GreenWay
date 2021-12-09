aa46b2b023206fae03a8dc5ad52392b134d2c592
Diogo Massuça <90829219+DiogoMassuca@users.noreply.github.com>
2021-12-09 11:58:02 +0000

    c

--- src/main/java/com/example/p1/models/views/RestauranteView.java
+++ src/main/java/com/example/p1/models/views/RestauranteView.java
@@ -2,4 +2,6 @@ package com.example.p1.models.views;
 
 public interface RestauranteView {
         String getRestauranteName();
+        Double getRestauranteLat();
+        Double getRestauranteLongt();
 }
