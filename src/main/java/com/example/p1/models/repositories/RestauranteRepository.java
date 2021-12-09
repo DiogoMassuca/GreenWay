ff0b743b7e7d51c53970a03bcc42aa58e18fb788
Diogo Massuça <90829219+DiogoMassuca@users.noreply.github.com>
2021-12-09 10:51:31 +0000

    c

--- src/main/java/com/example/p1/models/repositories/RestauranteRepository.java
+++ src/main/java/com/example/p1/models/repositories/RestauranteRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.repository.query.Param;
 public interface RestauranteRepository extends CrudRepository<Restaurante, Integer>{
         String resQuery1 = "SELECT res_name RestaurantName " +
                             "FROM restaurantes, ementas, pratos, alimentos, pratoalimento " +
-                            "WHERE eme_res_id = res_id AND eme_pra_id = pra_id AND pa_pra_id = pra_id AND pa_ali_id = ali_id ";
+                            "WHERE eme_res_id = res_id AND eme_pra_id = pra_id AND pa_pra_id = pra_id AND pa_ali_id = ali_id AND intol_ali_id = intol_id ";
                             
-                            @Query(value=resQuery1 + "AND ali_id<>:id", nativeQuery=true)
+                            @Query(value=resQuery1 + "AND intol_id<>:id", nativeQuery=true)
                             Iterable<RestauranteView>getResNoAli1(@Param ("id") int id);        
                             
     }
