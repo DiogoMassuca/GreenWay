a811b368e984266f8a0ac33646134ad5a7061a9d
Diogo Massuça <90829219+DiogoMassuca@users.noreply.github.com>
2021-12-16 09:27:41 +0000

    user post

--- src/main/java/com/example/p1/controllers/UserController.java
+++ src/main/java/com/example/p1/controllers/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,8 +44,8 @@ public class UserController {
         else return _user.get();
     }
     
-    @PostMapping(path = "/created", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void createUser(@RequestBody User user) {
-        Integer created = userRepository.createUser(user);
+    @PostMapping(path = "/{name}/{password}/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void createUser(@PathVariable String name, @PathVariable String password, @PathVariable String email) {
+        Integer created = userRepository.createUser(name, password, email);
     }
 }
